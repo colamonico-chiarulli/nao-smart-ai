52dbe58a3689a2686599d67ab2a9378dd7d75978
Rino Andriano <andriano@colamonicochiarulli.edu.it>
2026-01-22 13:41:34 +0100

    Adding landing vendor file

--- web_dashboard/Views/layout_dashboard.php
+++ web_dashboard/Views/layout_dashboard.php
@@ -57,12 +57,12 @@
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <link href="<?= url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?= url('assets/vendor/bootstrap-icons/bootstrap-icons.css') ?>" rel="stylesheet">
-    <link href="<?= url('assets/vendor/aos/aos.css') ?>" rel="stylesheet">
-    <link href="<?= url('assets/vendor/glightbox/css/glightbox.min.css') ?>" rel="stylesheet">
-    <link href="<?= url('assets/vendor/swiper/swiper-bundle.min.css') ?>" rel="stylesheet">
+    <!-- landing_vendor CSS Files -->
+    <link href="<?= url('assets/landing_vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?= url('assets/landing_vendor/bootstrap-icons/bootstrap-icons.css') ?>" rel="stylesheet">
+    <link href="<?= url('assets/landing_vendor/aos/aos.css') ?>" rel="stylesheet">
+    <link href="<?= url('assets/landing_vendor/glightbox/css/glightbox.min.css') ?>" rel="stylesheet">
+    <link href="<?= url('assets/landing_vendor/swiper/swiper-bundle.min.css') ?>" rel="stylesheet">
 
     <!-- Main CSS File -->
     <link href="<?= url('assets/css/main.css') ?>" rel="stylesheet">
@@ -158,13 +158,13 @@
     <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
-    <script src="<?= url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-    <script src="<?= url('assets/vendor/php-email-form/validate.js') ?>"></script>
-    <script src="<?= url('assets/vendor/aos/aos.js') ?>"></script>
-    <script src="<?= url('assets/vendor/glightbox/js/glightbox.min.js') ?>"></script>
-    <script src="<?= url('assets/vendor/swiper/swiper-bundle.min.js') ?>"></script>
-    <script src="<?= url('assets/vendor/purecounter/purecounter_vanilla.js') ?>"></script>
+    <!-- landing_vendor JS Files -->
+    <script src="<?= url('assets/landing_vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= url('assets/landing_vendor/php-email-form/validate.js') ?>"></script>
+    <script src="<?= url('assets/landing_vendor/aos/aos.js') ?>"></script>
+    <script src="<?= url('assets/landing_vendor/glightbox/js/glightbox.min.js') ?>"></script>
+    <script src="<?= url('assets/landing_vendor/swiper/swiper-bundle.min.js') ?>"></script>
+    <script src="<?= url('assets/landing_vendor/purecounter/purecounter_vanilla.js') ?>"></script>
 
     <!-- Main JS File -->
     <script src="<?= url('assets/js/main.js') ?>"></script>
